a45fa06aac7a2ab896249c1cdb6cca08c49649ef
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-11-19 21:31:30 -0500

    Write conditional logic for nouns, add gitignore

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def home():
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == "POST":
-        return get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
+        result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
                               mood = request.form["mood"], person = request.form["person"], tense = request.form["tense"],
                               voice = request.form["voice"], degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
+        return render_template("form.html", result = result)
     else:
-        return render_template("form.html")
+        return render_template("form.html", result = "")
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
---
+++ static/app.js
@@ -0,0 +1,8 @@
+function posCheck(that) {
+    if (that.value == "noun") {
+    alert("check");
+        document.getElementById("noun").style.display = "block";
+    } else {
+        document.getElementById("noun").style.display = "none";
+    }
+}
