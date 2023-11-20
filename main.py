3fb0fb12a4f545ebe0fb719d45a8b45ae8772384
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-11-19 22:59:20 -0500

    Remove adverb, add conditional logic per POS for get form

--- main.py
+++ main.py
@@ -10,12 +10,38 @@ def home():
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == "POST":
-        result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
-                              mood = request.form["mood"], person = request.form["person"], tense = request.form["tense"],
-                              voice = request.form["voice"], degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
+
+        if request.form["wanted_pos"] == "noun": #takes gender, number, case
+            result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
+                                    mood = "NULL", person = "NULL", tense = "NULL",
+                                    voice = "NULL", degree = "NULL", wanted_pos = request.form["wanted_pos"])
+            
+        elif request.form["wanted_pos"] == "verb": #takes number, mood, person, tense, voice
+            result = get_latin_form(request.form["wd"], case = "NULL", number = request.form["number"], gender = "NULL",
+                                    mood = request.form["mood"], person = request.form["person"], tense = request.form["tense"],
+                                    voice = request.form["voice"], degree = "NULL", wanted_pos = request.form["wanted_pos"])
+
+        """
+        elif request.form["wanted_pos"] == "adverb": #takes degree
+            result = get_latin_form(request.form["wd"], case = "NULL", number = "NULL", gender = "NULL",
+                                    mood = "NULL", person = "NULL", tense = "NULL",
+                                    voice = "NULL", degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
+            
+        elif request.form["wanted_pos"] == "adjective": #takes gender, number, case, degree
+            result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
+                                    mood = "NULL", person = "NULL", tense = "NULL",
+                                    voice = "NULL", degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
+        """
+        
         return render_template("form.html", result = result)
     else:
         return render_template("form.html", result = "")
+    
+"""
+result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
+        mood = request.form["mood"], person = request.form["person"], tense = request.form["tense"],
+        voice = request.form["voice"], degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
+"""
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
