df1c13b0772cafbc9c13559851413d18ff48e6a8
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-11-20 00:30:11 -0500

    Fix posCheck (I don't know JS oops)

--- static/app.js
+++ static/app.js
@@ -1,36 +1,15 @@
 function posCheck(that) {
-    if (that.value == "noun") {
-        document.getElementById("noun").style.display = "block";
-        // hide all others
-        document.getElementById("verb").style.display = "none";
-        document.getElementById("adverb").style.display = "none";
-        document.getElementById("adjective").style.display = "none";
+    // hide all elements
+    document.getElementById("noun").style.display = "none";
+    document.getElementById("verb").style.display = "none";
+    document.getElementById("adjective").style.display = "none";
 
+    if (that.value == "noun") {
+        // show the noun element
+        document.getElementById("noun").style.display = "block";
     } else if (that.value == "verb") {
         document.getElementById("verb").style.display = "block";
-        // hide all others
-        document.getElementById("noun").style.display = "none";
-        document.getElementById("adverb").style.display = "none";
-        document.getElementById("adjective").style.display = "none";
-
-    } else if (that.value == "adverb") {
-        document.getElementById("adverb").style.display = "block";
-        // hide all others
-        document.getElementById("noun").style.display = "none";
-        document.getElementById("verb").style.display = "none";
-        document.getElementById("adjective").style.display = "none";
-
     } else if (that.value == "adjective") {
         document.getElementById("adjective").style.display = "block";
-        // hide all others
-        document.getElementById("noun").style.display = "none";
-        document.getElementById("verb").style.display = "none";
-        document.getElementById("adverb").style.display = "none";
-    } else {
-        // hide all
-        document.getElementById("noun").style.display = "none";
-        document.getElementById("verb").style.display = "none";
-        document.getElementById("adverb").style.display = "none";
-        document.getElementById("adjective").style.display = "none";
     }
 }
