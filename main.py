a0066b8fcae489909b49c57845cf4ad90a882690
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-11-19 23:21:10 -0500

    Show input word next to output word

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def form():
                                     mood = "NULL", person = "NULL", tense = "NULL",
                                     voice = "NULL", degree = request.form["degree"], wanted_pos = request.form["wanted_pos"])
         """
-        
-        return render_template("form.html", result = result)
+
+        return render_template("form.html", query = request.form["wd"], arrow = "=>", result = result)
     else:
-        return render_template("form.html", result = "")
+        return render_template("form.html", query = "", arrow = "", result = "")
     
 """
 result = get_latin_form(request.form["wd"], case = request.form["case"], number = request.form["number"], gender = request.form["gender"],
