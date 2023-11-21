d627fb5e6570bebffb55006946738610f3917af1
violet edwards <113566839+violaflora@users.noreply.github.com>
2023-11-21 15:45:32 -0500

    Change select IDs to classes

--- static/app.js
+++ static/app.js
@@ -1,16 +1,16 @@
 function posCheck(that) {
     // hide all elements
-    document.getElementById("noun").style.display = "none";
-    document.getElementById("verb").style.display = "none";
-    document.getElementById("adjective").style.display = "none";
+    document.getElementsByClassName("noun")[0].style.display = "none";
+    document.getElementsByClassName("verb")[0].style.display = "none";
+    document.getElementsByClassName("adjective")[0].style.display = "none";
 
     if (that.value == "noun") {
         // show the noun element
-        document.getElementById("noun").style.display = "block";
+        document.getElementsByClassName("noun")[0].style.display = "block";
     } else if (that.value == "verb") {
-        document.getElementById("verb").style.display = "block";
+        document.getElementsByClassName("verb")[0].style.display = "block";
     } else if (that.value == "adjective") {
-        document.getElementById("adjective").style.display = "block";
+        document.getElementsByClassName("adjective")[0].style.display = "block";
     }
 }
 
